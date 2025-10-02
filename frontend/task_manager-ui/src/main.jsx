e8625e680e763e25188435ad3f2fb22eb00f713d
kumarharshit0413 <kumarharshit0413@gmail.com>
2025-10-02 11:14:26 +0530

    feat: Add .npmrc to fix Vercel build

--- frontend/task_manager-ui/src/main.jsx
+++ frontend/task_manager-ui/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://task-manager-api-i9s5.onrender.com/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
