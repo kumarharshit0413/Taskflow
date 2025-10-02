cee7359f1cc0c0df5134a77ffb1cd4696d05da8f
kumarharshit0413 <kumarharshit0413@gmail.com>
2025-10-02 11:33:17 +0530

    feat: Update CORS for production

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,9 @@ connectDB();
 
 const app = express();
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://your-frontend-url.vercel.app'],
+  origin: ['http://localhost:5173',
+    'https://taskflow-ruddy-rho.vercel.app' 
+  ],
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
